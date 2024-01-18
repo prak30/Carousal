20fb5d764d88907516a9957cfff2a49a5ecfb510
pranav <katkarpranav4@gmail.com>
2024-01-19 02:12:24 +0530

    changed background color

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import ImageSlider from "./component/ImageSlider";
 import Header from "./component/Header";
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-teal-100 min-h-screen">
       <Header />
       <ImageSlider />
     </div>
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="text-center m-4 p-5">
+    <div className="text-center p-5">
       <h1 className="text-bold text-4xl">ImageSlider</h1>
     </div>
   );
