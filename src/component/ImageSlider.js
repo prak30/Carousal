b934360af8718b3c77870c0524ed2b88a722a8c3
pranav <katkarpranav4@gmail.com>
2024-01-19 14:16:50 +0530

    Added our hardcoded data in utils

--- src/component/ImageSlider.js
+++ src/component/ImageSlider.js
@@ -1,19 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { data } from "../utils/constants";
 
 const ImageSlider = () => {
-  const data = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzUfQ8GDTD7ub8ictkguYlYS-0lErBQdJYog&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLhrj_nRybqRs--_aXsOgwTz8PveTTdt_CVA&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhRbrbyxiEwJml8H2oXB5UL3E7Me4ZebBf3DoYNGk5RVj0P0TnQ5Hf6HJz-IVchIEyHc8&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv3ALFuRXIAcdGJ6na4pHVgHqd7-Xjkgrl3w&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQynWU_TVIrJy8hkV0J-C6if7iSC3fbuk1crDEMTrRIMmuqTmGlf5J3dSpSSAwdbNFO_38&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuYrLDJAD3RBKOWh1f4cC8sLNGxMd6K_VQDw&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiAsQCAIkcsgKDWDpqihB2bzIsyfxUnLQUog&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsOsVrOcN7pQhUrNlzCa6WOg9K9siPyTbwiw&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoElfe-j7IMKr-Axj2-LzPLyrJEGMoRgojDA&usqp=CAU",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_eNUC7BRwHHoRKYdN3AYR_h2fvSNXrOaWJQ&usqp=CAU",
-  ];
   const [currentImg, setCurrentImg] = useState(0);
   const handlePreviousClick = () => {
     {
